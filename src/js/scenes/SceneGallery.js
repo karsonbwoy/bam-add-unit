764eff96d52cc35f239bfe8cee0136744c2a2c7f
karsonbwoy <skarapyta@gmail.com>
2025-07-02 15:05:48 +0200

    enchance gallery with swiper, pass position index from clicked gallery item to video

--- src/js/scenes/SceneGallery.js
+++ src/js/scenes/SceneGallery.js
@@ -1,12 +1,23 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
+
 export const gallery = document.getElementById('scene-gallery');
 
 const showGallery = (showVideo) => {
     gallery.classList.remove('hidden');
-    gallery.addEventListener('click', () => {
-        let position = 1
-        gallery.classList.add('hidden');
-        showVideo(position)
-    })
+
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        loop: true,
+    });
+
+    document.querySelectorAll('.swiper-slide').forEach((slide, index) => {
+        slide.addEventListener('click', () => {
+            gallery.classList.add('hidden');
+            showVideo(index + 1);
+        });
+    });
+
 }
 
 export default showGallery;
--- src/js/scenes/SceneVideo.js
+++ src/js/scenes/SceneVideo.js
@@ -1,7 +1,11 @@
-const video = document.getElementById('scene-video');
-const showVideo = (position) => {
-    video.classList.remove('hidden');
-    console.log(position)
+const sceneVideo = document.getElementById('scene-video');
+const video = sceneVideo.children[0];
+
+const positions = ['top-left', 'top-right', 'bottom-left', 'bottom-right'];
+
+const showVideo = (positionIndex) => {
+    sceneVideo.classList.remove('hidden');
+    video.classList.add(positions[positionIndex - 1]);
 }
 
 export default showVideo;
