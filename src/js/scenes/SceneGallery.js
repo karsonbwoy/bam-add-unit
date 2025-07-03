43826d2cbaa3c3f8e6e8cfa71028a59ed4aaa565
karsonbwoy <skarapyta@gmail.com>
2025-07-03 18:04:55 +0200

    Change swiper animation and add pulsing to cta button

--- src/js/scenes/SceneGallery.js
+++ src/js/scenes/SceneGallery.js
@@ -1,17 +1,35 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
+import gsap from 'gsap';
 
 export const gallery = document.getElementById('scene-gallery');
 
 const showGallery = (showVideo) => {
     gallery.classList.remove('hidden');
-
+    let tween = gsap.to("#cta-button", { scale: 1.1, duration: 0.5, yoyo: true, repeat: -1 });
+    tween.play();
     const swiper = new Swiper('.swiper', {
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        effect: 'coverflow',
+        coverflowEffect: {
+            slideShadows: false,
+        },
+        breakpoints: {
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+        },
+        speed: 600,
         slidesPerView: 1,
         spaceBetween: 10,
         centeredSlides: true,
         loop: true,
     });
+
 
     document.querySelectorAll('.swiper-slide').forEach((slide, index) => {
         slide.addEventListener('click', () => {
